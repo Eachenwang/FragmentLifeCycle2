789dea2496e0f6c9a1d2137e8e181f45c4101997
wangyichen <ethan.wake@gmail.com>
2018-08-10 05:14:58 +0800

    12-5-1 同位置使用不同的 Fragment 設計新類別 SwapActivity

---
+++ app/src/main/java/com/eth/fragmentlifecycle2/SwapActivity.java
@@ -0,0 +1,32 @@
+package com.eth.fragmentlifecycle2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SwapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swap);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swap();
+            }
+        });
+    }
+
+    private void swap() {
+
+    }
+
+}
